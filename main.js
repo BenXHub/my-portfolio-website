82d34bb5f00a040b6bd65230733e03666dcec2ee
benjamineke <90320051+benjamineke@users.noreply.github.com>
2023-08-19 23:45:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,30 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemenu = document.getElementById("sideMenu");
+
+function openMenu() {
+    sidemenu.style.right = "0";
+}
+function closeMenu() {
+    sidemenu.style.right = "-200px";
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzdjdsd0EGhXRdQ0N7BILMwUmmvigWsoxE9dQVOFJ-BdEPhCVFh-XMzLNLYn4PL6APF/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully!"
+            setTimeout(function (){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+    .catch(error => console.error('Error!', error.message))
+})
